edd2508c5bc1d02be69b47f4fac49251fb3805ed
pantunelfreddy <pantun.elfreddy@ui.ac.id>
2022-09-14 11:31:38 +0700

    Menyelesaikan tugas2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
+from multiprocessing import context
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang_katalog' : data_barang_katalog,
+        'nama': 'Pantun Elfreddy',
+        'NPM' : '2106751612',
+        }
+    return render(request, "katalog.html", context)
